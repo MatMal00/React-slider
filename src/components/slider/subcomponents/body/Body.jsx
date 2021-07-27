9f4fa357d26ba377496f66e9e5a3bc074624c1c3
Mateusz Malec <m.mateusz2000@wp.pl>
2021-07-27 21:58:08 +0200

    fix: animation

--- src/components/slider/subcomponents/body/Body.jsx
+++ src/components/slider/subcomponents/body/Body.jsx
@@ -3,7 +3,7 @@ import styles from './Body.module.css';
 const Body = ({ images }) => {
     const renderImage = images.map(img => (
         <img
-            key={img.src}
+            key={Math.random()}
             className={img.active ? `${styles.active}` : `${styles.hide}`}
             src={img.src}
             alt={img.alt}
